afe3f063e8aac2627ba1f184c397a1e75679092f
klfischer89 <fischer-k4@web.de>
2025-12-10 15:46:33 +0100

    calculate precision and recall using threshold for data

---
+++ project_diabetes.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import recall_score, precision_score, plot_confusion_matrix
+
+df = pd.read_csv("./data/diabetes.csv")
+
+
+X = df[["BMI", "Age", "Glucose", "BloodPressure"]]
+y = df["Outcome"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.75)
+
+model = LogisticRegression(class_weight = {0: 1, 1: 5})
+model.fit(X_train, y_train)
+
+model.predict_proba(X_test)[:, 1] >= 0.3
+
+y_test_pred = model.predict_proba(X_test)[:, 1] >= 0.5
+
+precision = precision_score(y_test, y_test_pred)
+recall = recall_score(y_test, y_test_pred)
+
+print("Precision: " + str(precision))
+print("Recall: " + str(recall))
